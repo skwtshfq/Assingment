f7e3d806d3979ecb63dd3bddc92b7bccbd377054
Md Sakhawat Hossen <131347580+skwtshfq@users.noreply.github.com>
2023-07-15 10:23:56 +0600

    Add files via upload

---
+++ C-9-T-2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+  double a; cin>>a;
+  double ans =  3.14159*a*a;
+  cout<<"A=";
+  cout<<fixed<<setprecision(4)<<ans<<endl;
+    return 0;
+}
