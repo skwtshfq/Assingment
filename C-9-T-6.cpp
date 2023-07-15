8fcf46687e26397671e14385c836c46bd744be5c
Md Sakhawat Hossen <131347580+skwtshfq@users.noreply.github.com>
2023-07-15 11:13:29 +0600

    Add files via upload

---
+++ C-9-T-6.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+  double a,b,c; cin>>a>>b>>c;
+  double x= a*2+b*3+c*5;
+  double ans= x/10.0;
+  cout<<"MEDIA = "<<fixed<<setprecision(1)<<ans<<endl;
+    return 0;
+}
+
